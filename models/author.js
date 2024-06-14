a0ad1aab6ed22942f570891c3fddbdb0e7efe4c1
RWPmadeZaeem <mkhalid.bscs21seecs@seecs.edu.pk>
2024-06-15 02:31:21 +0500

    update and delete

--- models/author.js
+++ models/author.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const { DateTime } = require("luxon");
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
@@ -27,6 +27,11 @@ AuthorSchema.virtual("name").get(function () {
     // We don't use an arrow function as we'll need the this object
     return `/catalog/author/${this._id}`;
   });
+ 
+  AuthorSchema.virtual("lifespan").get(function () { 
+    return `${this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : ""} - ${this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : ""}`;
+  });
+
   
 
 const Author = mongoose.model('Author', AuthorSchema);
